8cb91d11b886cc131958c5c4c2455d0cee3888ec
Stephan Schmid <stephan.schmid@sunware.ch>
2024-09-02 15:58:37 +0200

    friendrequests

--- src/components/FriendRequestList.tsx
+++ src/components/FriendRequestList.tsx
@@ -1,11 +1,81 @@
 "use client";
 
+import { FollowRequest, User } from "@prisma/client";
+import Image from "next/image";
 import { useOptimistic, useState } from "react";
 
-export const FriendRequestList = () => {
+import { acceptFollowRequest, declineFollowRequest } from "@/lib/actions";
+
+type RequestWithUser = FollowRequest & {
+  sender: User;
+};
+
+export const FriendRequestList = ({ requests }: { requests: RequestWithUser[] }) => {
+  const [requestState, setRequestState] = useState(requests);
+
+  const accept = async (requestId: string, userId: string) => {
+    removeOptimisticRequest(requestId);
+    try {
+      await acceptFollowRequest(userId);
+      setRequestState((prev) => prev.filter((req) => req.id !== requestId));
+    } catch (err) {}
+  };
+  const decline = async (requestId: string, userId: string) => {
+    removeOptimisticRequest(requestId);
+    try {
+      await declineFollowRequest(userId);
+      setRequestState((prev) => prev.filter((req) => req.id !== requestId));
+    } catch (err) {}
+  };
+  const [optimisticRequests, removeOptimisticRequest] = useOptimistic(
+    requestState,
+    (state, value: string) => state.filter((req) => req.id !== value)
+  );
+
   return (
     <div>
-      <h1>FriendRequestList</h1>
+      {optimisticRequests.map((request) => (
+        <div className="flex items-center justify-between" key={request.id}>
+          <div className="flex items-center gap-4 mb-1">
+            <Image
+              src={request.sender.avatar || "/noAvatar.png"}
+              alt=""
+              width={40}
+              height={40}
+              className="w-10 h-10 rounded-full object-fill"
+            />
+            <span className="text-sm">
+              {request.sender.name && request.sender.surname
+                ? request.sender.surname + " " + request.sender.name
+                : request.sender.username}
+            </span>
+          </div>
+        <div className="flex gap-3 justify-end">
+          <form action={() => accept(request.id, request.sender.userId)}>
+            <button>
+              <Image
+                src="/accept.png"
+                alt=""
+                width={20}
+                height={20}
+                className="cursor-pointer"
+              />
+            </button>
+          </form>
+          <form action={() => decline(request.id, request.sender.userId)}>
+            <button>
+              <Image
+                src="/reject.png"
+                alt=""
+                width={20}
+                height={20}
+                className="cursor-pointer"
+              />
+            </button>
+          </form>
+        </div>
+      </div>        
+      ))}
     </div>
   )
 };
--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -1,7 +1,7 @@
 import prisma from "@/lib/client";
 import { auth } from "@clerk/nextjs/server";
-import Image from "next/image"
 import Link from "next/link"
+import { FriendRequestList } from "./FriendRequestList";
 
 export const FriendRequests = async () => {
   const {userId: currentUserId} = auth();
@@ -27,29 +27,7 @@ export const FriendRequests = async () => {
         <Link href="/" className="text-blue-500 text-xs">See all</Link>
       </div>
       { /* USER */}
-      {requests.map((request) => (
-        <div 
-          key={request.id}
-          className="flex items-center justify-between"
-        >
-            <div className="flex items-center gap-4">
-              <Image 
-                src={request.sender.avatar || "/noAvatar.png"}
-                alt=""
-                width={40}
-                height={40}
-                className="w-10 h-10 rounded-full object-fill"
-              />
-              <span className="text-sm">
-                {request.sender.surname && request.sender.name ? request.sender.surname + " " + request.sender.name : request.sender.username}
-              </span>
-            </div>
-            <div className="flex gap-3 justify-end">
-              <Image src="/accept.png" alt="" width={20} height={20} className="cursor-pointer"/>
-              <Image src="/reject.png" alt="" width={20} height={20} className="cursor-pointer"/>
-            </div>
-        </div>
-      ))}
+      <FriendRequestList requests={requests} />
       { /* BOTTOM */}
       <div className="">
 
