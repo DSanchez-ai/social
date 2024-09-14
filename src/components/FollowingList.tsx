d273228ae28bce4d585c779a16bb91802ec1ab0e
Stephan Schmid <stephan.schmid@sunware.ch>
2024-09-14 16:10:51 +0200

    Lists

--- src/components/FollowingList.tsx
+++ src/components/FollowingList.tsx
@@ -19,7 +19,7 @@ export const FollowingList = ({requests}:{requests: FollowingWithUser[]}) => {
   return (
     <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-2">
       <div className="flex justify-between items-center font-medium">
-        <span className="text-gray-500">Followers</span>
+        <span className="text-gray-500">Following</span>
         <span 
           className="text-blue-500 text-xs cursor-pointer"
           onClick={() => setShowAll(!showAll)}
