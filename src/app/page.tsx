8f391f5496f1096fffd8f91492ec9e851e685997
Stephan Schmid <stephan.schmid@sunware.ch>
2024-10-05 14:41:02 +0200

    pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,48 @@ import { Followers } from "@/components/Followers"
 import { Following } from "@/components/Following"
 import { FriendRequests } from "@/components/FriendRequests"
 import { LeftMenu } from "@/components/LeftMenu"
+import Pagination from "@/components/Pagination"
+import { Post } from "@/components/Post"
 import { ProfileCard } from "@/components/ProfileCard"
 import { RightMenu } from "@/components/RightMenu"
 import { Stories } from "@/components/Stories"
+import prisma from "@/lib/client"
 
-const Homepage = () => {
+const numberOfPosts = 5;
 
+async function getData(searchParams: string) {
+  const [count, data] = await prisma.$transaction([
+  prisma.post.count(),
+  prisma.post.findMany({
+    take: numberOfPosts,
+    skip: searchParams ? (Number(searchParams) -1) * numberOfPosts : 0,
+    include: {
+      user: true,
+      likes: {
+        select: {
+          userId: true
+        }
+      },
+      _count: {
+        select: {
+          likes: true,
+          comments: true
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc"
+    },
+  }) 
+  ])
+  return {count, data}
+}
+
+export default function Homepage({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <div className='flex gap-6 pt-6'>
       {/* LEFT SIDE */}
@@ -35,8 +71,8 @@ const Homepage = () => {
           </div>
           <Stories />
           <AddPost />
-          <Feed />
-        </div>
+          <ShowItems searchParams={searchParams} />
+          </div>
       </div>  
       {/* RIGHT SIDE */}
       <div className="hidden lg:block w-[30%]">
@@ -46,4 +82,21 @@ const Homepage = () => {
   )
 }
 
-export default Homepage
+async function ShowItems({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  const {count, data} = await getData(searchParams.page);
+
+  return (
+    <>
+      {data.length ? (data.map(post=>(
+        // @ts-ignore
+        <Post key={post.id} post={post}/>
+      ))) : <span className="text-sm">No posts found!</span>}
+        <Pagination totalPages={Math.ceil(count / numberOfPosts)}/>  
+    </>
+  )
+}
+
